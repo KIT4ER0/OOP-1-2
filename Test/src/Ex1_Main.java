aad3b3bd423a164239892313e5353dd3c4a4b372
KIT4ER0 <112687445+KIT4ER0@users.noreply.github.com>
2023-02-23 00:22:34 +0700

    Delete Test directory

--- Test/src/Ex1_Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Ex1_Main {
-    public static void main(String[] args) {
-        Ex1_Student s = new Ex1_Student();
-        s.mScore = 80;
-        s.fScore = 80;
-        s.showGrade();
-}
-}
